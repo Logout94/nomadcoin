6ba921efbb1a4e64324b1544f9a381632e0ba019
Logout94 <leetool94@naver.com>
2021-07-13 00:20:59 +0900

    Using Printf to format different type such as binary

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import "fmt"
 
-func plus(a ...int) int {
-	var total int
-	for _, item := range a { // _ = ignore
-		total += item
-	}
-	return total
-}
-
 func main() {
-	result := plus(2, 3, 4, 5, 6, 7, 8, 9, 10)
-	fmt.Println(result)
+	name := "Nicolas!!!!!!! Is my name"
+	for _, letter := range name {
+		//fmt.Println(letter)
+		fmt.Printf("%b\n", letter)
+	}
 }
