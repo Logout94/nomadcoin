116af502a49f4dc36e8e53fdf8632ea33922f2c4
Logout94 <leetool94@naver.com>
2021-07-21 00:20:45 +0900

    Making receive function, p is copy

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/nomadcoders/nomadcoin/person"
+import (
+	"fmt"
+
+	"github.com/nomadcoders/nomadcoin/person"
+)
 
 func main() {
 	nico := person.Person{}
+	nico.SetDetails("nico", 12)
+	fmt.Println("Main 'nico':", nico)
 }
