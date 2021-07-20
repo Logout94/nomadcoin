064afeb514b87a37596c0997c9e27cb699fb6a13
Logout94 <leetool94@naver.com>
2021-07-21 00:54:00 +0900

    My first block, many block uses sha256 algorithm, point is that previous hash and current data make new current hash

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+/*
+B1
+
+	b1Hash = (data + "")
+
+B2
+	b2Hash = (data + b1Hash)
+
+B3
+	b3Hash = (data + b2Hash)
+*/
+
 func main() {
-	var name string = "nico"
+	genesisBlock := block{"Genesis Block", "", ""} // prevhash and has not exist
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+
+	hexHash := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hexHash
+	fmt.Println(genesisBlock)
 }
