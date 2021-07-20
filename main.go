d4721debb663840497f4772aeb70d692846a4082
Logout94 <leetool94@naver.com>
2021-07-21 00:14:43 +0900

    When using import, we have to use upper case

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import "fmt"
+import "github.com/nomadcoders/nomadcoin/person"
 
-type person struct {
-	name string
-	age  int
-}
-
-func (p person) sayHello() {
-	fmt.Printf("Hello My name is %s and I'm %d\n", p.name, p.age)
-}
-
-func (p person) KoreanAge() {
-	fmt.Printf("My Korean age is %d.\n", p.age)
-}
 func main() {
-	nico := person{name: "nico", age: 12}
-	nico.sayHello()
-	nico.KoreanAge()
+	nico := person.Person{}
 }
